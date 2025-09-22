fd866a78836627258d1ae818481ff93cd445d735
dora orak <doraorak@gmail.com>
2025-09-22 13:50:00 +0300

    add value viewer for entitlements

---
+++ EntDump/PopoverButton.h
@@ -0,0 +1,16 @@
+//
+//  PopoverButton.h
+//  EntDump
+//
+//  Created by Dora Orak on 22.09.2025.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PopoverButton : NSButton
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ EntDump/PopoverButton.m
@@ -0,0 +1,18 @@
+//
+//  PopoverButton.m
+//  EntDump
+//
+//  Created by Dora Orak on 22.09.2025.
+//
+
+#import "PopoverButton.h"
+
+@implementation PopoverButton
+
+- (void)drawRect:(NSRect)dirtyRect {
+    [super drawRect:dirtyRect];
+    
+    // Drawing code here.
+}
+
+@end
